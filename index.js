3dc00919a6771f5f92ffbf1171636d575cd53399
Edgar Albalate <dev.gardo@gmail.com>
2018-01-24 18:15:57 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const {json, buffer, text} = require('micro')
+const urlencoded = require('urlencoded-body-parser')
+
+module.exports = async (req) => {
+  switch (req.headers['content-type']){
+    case 'application/json':
+      return await json(req)
+    case 'application/x-www-form-urlencoded':
+      return await urlencoded(req)
+    default:
+      throw new Error(`Can't parse {req.headers['content-type']}`)
+  }
+}
